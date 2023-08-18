1174630b53d08201bdebe9d610ce1b142c90b9db
soujanvangapandu <vangapandusoujan6@gmail.com>
2023-08-18 13:18:01 +0530

    terraform-v1

--- roboshop-v1/roboshop.tf
+++ roboshop-v1/roboshop.tf
@@ -1,8 +1,19 @@
 resource "aws_instance" "frontend" {
   ami           = "ami-03265a0778a880afb"
   instance_type = "t2.micro"
+  vpc_security_group_ids = [ "sg-0523b29cdf9780811" ]
 
   tags = {
     Name = "frontend"
+  }
+}
+
+resource "aws_instance" "mongodb" {
+  ami           = "ami-03265a0778a880afb"
+  instance_type = "t2.micro"
+  vpc_security_group_ids = [ "sg-0523b29cdf9780811" ]
+
+  tags = {
+    Name = "mongodb"
   }
 }
